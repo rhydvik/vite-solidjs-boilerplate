59ef5b0457cbf15917cc7199beaafdaf22c52144
VIKRAM THAKUR <rhydvik@gmail.com>
2023-10-31 17:26:38 +0530

    test pre-commit hooks on pre-install

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -10,7 +10,7 @@ export type Props = {
 function Button({ variant, label, sx }: Props) {
   return (
     <SButton variant={variant} sx={sx}>
-      <div>{label}</div>
+      <div>test{label}</div>
     </SButton>
   );
 }
