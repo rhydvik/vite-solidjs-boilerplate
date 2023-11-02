e8340af4959127881c68827c9906762ff32473ad
Akash Pandey <akashpandey@Akashs-MacBook-Pro.local>
2023-11-02 19:29:31 +0530

    V4-0000 store implementation initital

--- src/views/offices/Offices.tsx
+++ src/views/offices/Offices.tsx
@@ -1,46 +1,27 @@
+/* eslint-disable  */
 import { A } from '@solidjs/router';
-import { createEffect, createSignal } from 'solid-js';
+import { createEffect, onMount } from 'solid-js';
 
 import { printLog } from '../../utils/utils';
 import { BasicModal } from '../../components';
-
-type Office = {
-  name: string;
-  location: string;
-};
-
+import { OfficeState } from '../../store';
 export default function Offices() {
-  const [data, setData] = createSignal<Office[]>([]);
-  const [loading, setLoading] = createSignal(true);
-
-  async function fetchData() {
-    try {
-      const response = await fetch('/api/offices');
-      const result = (await response.json()) as { offices: Office[] | [] };
-      setData(result.offices);
-      setLoading(false);
-    } catch (err) {
-      printLog('Error fetching data', err);
-      setLoading(false);
-    }
-  }
-
-  createEffect(async () => {
-    await fetchData();
-  });
-
+  
   // this kind of conditions messes up with solid reactivity
   // if (loading()) {
   //   return <p>Loading...</p>;
   // }
+  onMount(() => {
+    OfficeState.fetchAndSetOffices();
+  });
 
   return (
     <div>
       <h1>Offices</h1>
       <A href="/">Home</A>
-      {loading() && <p>Loading...</p>}
+      {OfficeState.officesStore.isLoading && <p>Loading...</p>}
       <ul>
-        {data().map((item) => {
+        {OfficeState.officesStore.offices.map((item:any) => {
           return <li>{item.name}</li>;
         })}
       </ul>
