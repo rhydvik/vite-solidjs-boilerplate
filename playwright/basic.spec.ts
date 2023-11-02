95a1aed5622af24ec426d5a98bded21e3a753f6b
Vikram Thakur <vikramthakur@Vikrams-Mac-mini.local>
2023-11-02 19:00:00 +0530

    playwright updated test

--- playwright/basic.spec.ts
+++ playwright/basic.spec.ts
@@ -1,8 +1,6 @@
 import { test, expect } from '@playwright/test';
 
 test.describe('Home Component', () => {
-  // This assumes that your SolidJS app is served at localhost:3000
-  // Adjust the URL as necessary.
   test.beforeEach(async ({ page }) => {
     await page.goto('http://localhost:3000/');
   });
@@ -20,12 +18,5 @@ test.describe('Home Component', () => {
   test('should have a button that can be clicked', async ({ page }) => {
     const button = page.locator('button:has-text("Check this")');
     await expect(button).toBeVisible();
-    // If you want to click the button and check for navigation or other side effects, you can do so here.
-    // For example, to click and wait for navigation:
-    // await Promise.all([
-    //   page.waitForNavigation(), // Waits for the next navigation
-    //   button.click(), // Clicks the button to trigger navigation
-    // ]);
-    // Then check the URL or other changes on the page.
   });
 });
