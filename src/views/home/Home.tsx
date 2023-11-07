3fba0cad2d683a6ff91de20d750567b216b8c31c
Vikram <rhydvik@gmail.com>
2023-11-07 16:14:59 +0530

    env variables

--- src/views/home/Home.tsx
+++ src/views/home/Home.tsx
@@ -3,6 +3,7 @@ import { Link } from '@solidjs/router';
 const Home = () => {
   return (
     <div>
+      {process.env.ENV}
       <p> This is a typescript component </p>
       <Link href="/offices">Offices</Link>
     </div>
