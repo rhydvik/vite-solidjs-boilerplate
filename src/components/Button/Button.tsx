517bf993220e8e0dca9b066d4d885ab60f25694b
Gaurav Gautam <gaurav.g@armstrongtransport.com>
2023-11-15 21:03:53 +0530

    Added Button Component

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,18 +1,79 @@
-import { Button as SButton } from '@suid/material';
+import React from 'react';
+import { createSignal } from 'solid-js';
+import { Button as SButton, Menu, MenuItem } from '@suid/material';
 
 export type Props = {
-  class?: string;
+  className?: string;
   variant: 'contained' | 'outlined' | 'text';
+  size?: 'small' | 'medium' | 'large' | string;
   label: string;
-  sx?: unknown;
+  icon?: React.ReactNode;
+  iconAlignment?: 'start' | 'end' | string;
   onClick?: () => void;
+  disabled?: boolean;
+  href?: string;
+  dropdownItems?: string[]; // List of items for the dropdown
+  onDropdownItemClick?: (item: string) => void; // Callback for dropdown item click
 };
 
-function Button({ variant, label, sx, onClick }: Props) {
+function Button({
+  variant,
+  label,
+  icon,
+  iconAlignment,
+  className,
+  onClick,
+  disabled,
+  href,
+  dropdownItems,
+  onDropdownItemClick,
+}: Props) {
+  const [anchorEl, setAnchorEl] = createSignal<null | HTMLButtonElement>(null);
+  const [isDropdownOpen, setDropdownOpen] = createSignal(false);
+
+  const handleButtonClick = (
+    event: React.MouseEvent<HTMLButtonElement, MouseEvent>,
+  ) => {
+    setAnchorEl(event.currentTarget);
+    setDropdownOpen(!isDropdownOpen());
+    onClick && typeof onClick === 'function' && onClick(); // Check if onClick is a function before calling it
+  };
+
+  const handleDropdownItemClick = (item: string) => {
+    setAnchorEl(null);
+    setDropdownOpen(false);
+    onDropdownItemClick &&
+      typeof onDropdownItemClick === 'function' &&
+      onDropdownItemClick(item); // Check if onDropdownItemClick is a function before calling it
+  };
+
   return (
-    <SButton variant={variant} sx={sx} onClick={() => onClick && onClick()}>
-      <div>{label}</div>
-    </SButton>
+    <>
+      <SButton
+        variant={variant}
+        className={className}
+        onClick={handleButtonClick}
+        disabled={disabled}
+        href={href}
+        startIcon={iconAlignment === 'start' ? icon : null}
+        endIcon={iconAlignment === 'end' ? icon : null}
+      >
+        <div>{label}</div>
+      </SButton>
+      {dropdownItems && (
+        <Menu
+          anchorEl={anchorEl}
+          open={isDropdownOpen()}
+          onClose={() => setDropdownOpen(false)}
+        >
+          {dropdownItems.map((item) => (
+            <MenuItem key={item} onClick={() => handleDropdownItemClick(item)}>
+              {item}
+            </MenuItem>
+          ))}
+        </Menu>
+      )}
+    </>
   );
 }
 
--- src/stories/Button.stories.tsx
+++ src/stories/Button.stories.tsx
@@ -1,26 +1,63 @@
 import { JSX } from 'solid-js';
+import { Meta } from '@storybook/addon-docs/blocks';
 
-import { Button } from '../components/Button';
+import { Button, Props } from '../components/Button';
 
 export default {
-  title: 'Components/Button', // Define the story grouping and name
-  component: Button, // Specify the component for auto-generation of controls
+  title: 'Components/Button',
+  component: Button,
+  parameters: {
+    controls: { hideNoControlsWarning: true }, // To hide controls warning
+    docs: {
+      page: () => <Meta title="Button" />,
+    },
+  },
 };
 
-// Define a template for creating stories
-const Template: (
-  args: JSX.ButtonHTMLAttributes<HTMLButtonElement> & { children: JSX.Element },
-) => JSX.Element = (args) => <Button {...args} />;
+const Template: (args: Props) => JSX.Element = (args) => <Button {...args} />;
 
-// Create the actual stories
-export const Primary = Template.bind({});
-Primary.args = {
+export const Contained = Template.bind({});
+Contained.args = {
   variant: 'contained',
-  label: 'Primary Button',
+  label: 'Contained Button',
 };
 
-export const Secondary = Template.bind({});
-Secondary.args = {
+export const Outlined = Template.bind({});
+Outlined.args = {
+  variant: 'outlined',
+  label: 'Outlined Button',
+};
+
+export const Text = Template.bind({});
+Text.args = {
   variant: 'text',
-  label: 'Secondary Button',
+  label: 'Text Button',
+};
+
+export const WithIcon = Template.bind({});
+WithIcon.args = {
+  variant: 'contained',
+  label: 'Button with Icon',
+  icon: (
+    <span role="img" aria-label="icon">
+      &#10003;
+    </span>
+  ),
+  iconAlignment: 'start',
+};
+
+export const Disabled = Template.bind({});
+Disabled.args = {
+  variant: 'contained',
+  label: 'Disabled Button',
+  disabled: true,
+};
+
+export const WithDropdown = Template.bind({});
+WithDropdown.args = {
+  variant: 'contained',
+  label: 'Button with Dropdown',
+  dropdownItems: ['Item 1', 'Item 2', 'Item 3'],
+  // eslint-disable-next-line no-console
+  onDropdownItemClick: (item: string) => console.log(`Clicked: ${item}`),
 };
