fb778d84dcfdb1554d3e77df64eea3dab3f96b83
Gaurav Gautam <gaurav.g@armstrongtransport.com>
2023-11-24 17:51:10 +0530

    Update Click target for Card component

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -48,7 +48,6 @@ export default function Card({
   return (
     <MuiCard raised={raised} sx={cardStyles.card}>
       <CardHeader
-        onClick={accordion ? handleToggle : undefined}
         sx={cardStyles.cardHeader}
         title={
           <Grid
@@ -62,6 +61,7 @@ export default function Card({
                 variant="body2"
                 component="span"
                 sx={cardStyles.accordionIcon}
+                onClick={accordion ? handleToggle : undefined}
               >
                 {accordion &&
                   (isExpanded() ? (
