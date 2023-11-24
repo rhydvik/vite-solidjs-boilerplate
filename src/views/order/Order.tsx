6ea0932ab7c36b5cdb33c40de5e29a5c35e59782
Gaurav Gautam <gaurav.g@armstrongtransport.com>
2023-11-24 17:54:13 +0530

    Removed DriverAndTruckDetails components

--- src/views/order/Order.tsx
+++ src/views/order/Order.tsx
@@ -54,8 +54,6 @@ const Order = () => {
 
                 <LoadInformation />
 
-                <DriverAndTruckDetails />
-
                 <AdditionalLoadDetails />
 
                 <CarrierInstructions />
